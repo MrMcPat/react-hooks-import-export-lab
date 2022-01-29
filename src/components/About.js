beea889658c7f4357eebe842a08d5753b457b2dc
Patrick Liang <pl204511194@yahoo.com>
2022-01-29 10:25:10 -0500

    commit tree eed93e4a277393edd43c825851b57cceef4d287d

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {image, city, username} from "../data/user";
 
 function About() {
   return (
@@ -9,3 +10,5 @@ function About() {
     </div>
   );
 }
+
+export default About;
